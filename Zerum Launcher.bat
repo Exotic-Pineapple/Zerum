d68c6db4030b40f202a8097680f6922646d967e6
Exotic Pineapple <69941591+Exotic-Pineapple@users.noreply.github.com>
2022-11-29 18:18:50 -0600

    Delete Zerum Launcher.bat

--- Zerum Launcher.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-Title Launching Zerum...
-Start "" https://rsharmila20083.wixsite.com/zerum-uc
-Set /p Input="UC Code: "
-if "%input%"=="10010110 Fls" goto NU
-
-:NU
-START /MIN Zerum.py
-EXIT
